ec801a3f6d00c524a036f6b7160edf2db15356e9
vishnu <vishnuv.stackuptvm@gmail.com>
2025-09-30 08:06:07 +0530

    back

---
+++ src/components/Mesages/CommingSoon.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function ComingSoon() {
+  return (
+    <div className=" flex items-center justify-center p-4">
+      <div className="bg-purple-100 shadow-lg rounded-2xl px-8 py-4 text-center ">
+        <h1 className=" text-3xl sm:text-4xl font-bold text-gray-800">
+          🚀 Coming Soon
+        </h1>
+        <p className="mt-3 text-gray-500 text-sm sm:text-base">
+          Something amazing is on the way
+        </p>
+      </div>
+    </div>
+  );
+}
